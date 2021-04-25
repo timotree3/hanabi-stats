67ea102870ada1d48363bc2210ea521b175932be
timotree3 <timorcb@gmail.com>
2021-04-25 17:17:50 -0400

    Auto format

--- power_rankings.py
+++ power_rankings.py
@@ -4,25 +4,39 @@ num_games_per_player = {}
 games_per_player = {}
 
 # Load dataset
-with open('input/list_of_players.txt') as players:
+with open("input/list_of_players.txt") as players:
     for player in players:
         current_strength_per_player[player.strip()] = 0
     for player in current_strength_per_player:
         num_games = 0
         games_per_player[player] = []
-        with open('temp/{}_stats.txt'.format(player)) as player_history_file:
+        with open("temp/{}_stats.txt".format(player)) as player_history_file:
             for game in player_history_file:
-                game = game.strip().split('\t')
-                [replay, num_players, score, variant, time, players, other_games_same_seed, num_suits, max_score] = game
-                
+                game = game.strip().split("\t")
+                [
+                    replay,
+                    num_players,
+                    score,
+                    variant,
+                    time,
+                    players,
+                    other_games_same_seed,
+                    num_suits,
+                    max_score,
+                ] = game
+
                 fraction_max_score = int(score) / int(max_score)
-                players = players.split(', ')
+                players = players.split(", ")
                 if all([player in current_strength_per_player for player in players]):
-                    game = { 'fraction_max_score': fraction_max_score, 'variant': variant, 'players': players }
+                    game = {
+                        "fraction_max_score": fraction_max_score,
+                        "variant": variant,
+                        "players": players,
+                    }
                     games[replay] = game
                     num_games += 1
                     games_per_player[player].append(game)
-        
+
         num_games_per_player[player] = num_games
 
 
@@ -41,9 +55,9 @@ while min(num_games_per_player.values()) < 50:
     replays_to_remove = []
     for replay in games:
         game = games[replay]
-        if too_small_sample_size_player in game['players']:
+        if too_small_sample_size_player in game["players"]:
             replays_to_remove.append(replay)
-            for player in game['players']:
+            for player in game["players"]:
                 if player == too_small_sample_size_player:
                     continue
 
@@ -57,35 +71,39 @@ for player in current_strength_per_player:
     print(player, current_strength_per_player[player], num_games_per_player[player])
 
 for iteration in range(100):
-    sum_performances_per_player = dict([(player, 0) for player in current_strength_per_player])
+    sum_performances_per_player = dict(
+        [(player, 0) for player in current_strength_per_player]
+    )
     for replay in games:
         game = games[replay]
 
-        sum_strengths = sum([current_strength_per_player[player] for player in game['players']])
-        for alice in game['players']:
-            sum_strengths_excluding_alice = sum_strengths - current_strength_per_player[alice]
-            avg_strength_excluding_alice = sum_strengths_excluding_alice / (len(game['players']) - 1)
+        sum_strengths = sum(
+            [current_strength_per_player[player] for player in game["players"]]
+        )
+        for alice in game["players"]:
+            sum_strengths_excluding_alice = (
+                sum_strengths - current_strength_per_player[alice]
+            )
+            avg_strength_excluding_alice = sum_strengths_excluding_alice / (
+                len(game["players"]) - 1
+            )
 
-            alice_performance = game['fraction_max_score'] - avg_strength_excluding_alice
+            alice_performance = (
+                game["fraction_max_score"] - avg_strength_excluding_alice
+            )
 
             sum_performances_per_player[alice] += alice_performance
 
     next_strength_per_player = {}
     for player in sum_performances_per_player:
-        next_strength_per_player[player] = sum_performances_per_player[player] / num_games_per_player[player]
-
-    # # Normalize next strengths to be between 0 and 1
-    # min_next_strength = min(next_strength_per_player.values())
-    # max_next_strength = max(next_strength_per_player.values())
-    # next_strength_interval = max_next_strength - min_next_strength
-
-    # for player in next_strength_per_player:
-    #     next_strength_per_player[player] -= min_next_strength
-    #     next_strength_per_player[player] /= next_strength_interval
-    #     assert next_strength_per_player[player] >= 0 and next_strength_per_player[player] <= 1
+        next_strength_per_player[player] = (
+            sum_performances_per_player[player] / num_games_per_player[player]
+        )
 
     current_strength_per_player = next_strength_per_player
 
-    with open('output/iteration_{}.tsv'.format(iteration), 'w') as output:
-        for [player, current_strength] in sorted(current_strength_per_player.items(), key = lambda item: item[1], reverse = True):
-            output.write('{}\t{}\n'.format(player, current_strength))
+    with open("output/iteration_{}.tsv".format(iteration), "w") as output:
+        for [player, current_strength] in sorted(
+            current_strength_per_player.items(), key=lambda item: item[1], reverse=True
+        ):
+            output.write("{}\t{}\n".format(player, current_strength))
