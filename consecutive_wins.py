a4c89e05774cea48e473a2710132713a035b7b75
timotree3 <timorcb@gmail.com>
2021-05-07 10:10:55 -0400

    Add consecutive wins calculation

--- consecutive_wins.py
+++ consecutive_wins.py
@@ -9,8 +9,9 @@ with open("input/list_of_players.txt") as players:
             max_seq = 0
             max_seq_ender = None
             for line in f:
+                line = line.strip()
                 fields = line.split("\t")
-                if fields[2] == "0":
+                if fields[2] == fields[8]:
                     seq += 1
                 else:
                     if seq > max_seq:
@@ -23,7 +24,11 @@ with open("input/list_of_players.txt") as players:
 
 players_stats = sorted(players_stats, key=lambda player: player[1], reverse=True)
 
-with open("output/consecutive_strikeouts.tsv", "w") as output:
-    output.write("Player\tMax Consecutive Strikeouts\tStart of Sequence\n")
+with open("output/consecutive_wins.tsv", "w") as output:
+    output.write("Player\tMax Consecutive Wins\tStart of Sequence\n")
     for [player, max_seq, max_seq_ender] in players_stats:
-        output.write("{}\t{}\t{}\n".format(player, max_seq, max_seq_ender[0]))
+        output.write(
+            "{}\t{}\t{}\n".format(
+                player, max_seq, max_seq_ender[0] if max_seq_ender else "N/A"
+            )
+        )
